ffb8d26f52329f54e2e162be384880ec3c3b9d2c
Lokesh7688 <lokesh0kashyap@gmail.com>
2020-08-14 22:00:29 +0530

    issue on line 38

--- lib/Reels.dart
+++ lib/Reels.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_insta/flutter_insta.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
-import 'package:http/http.dart';
 
 class ReelsDownload extends StatefulWidget {
   @override
@@ -9,6 +8,48 @@ class ReelsDownload extends StatefulWidget {
 }
 
 class _ReelsDownloadState extends State<ReelsDownload> {
+  FlutterInsta flutterInsta =
+      FlutterInsta(); // create instance of FlutterInsta class
+  TextEditingController reelController = TextEditingController();
+
+  bool downloading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    InitializeDownloader();
+    downloadReels();
+  }
+
+  void InitializeDownloader() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    FlutterDownloader.initialize(
+        debug: true // optional: set false to disable printing logs to console
+        );
+  }
+
+  void downloadReels() async {
+    String s = await flutterInsta
+        .downloadReels("https://www.instagram.com/p/CDlGkdZgB2y");
+    print(s);
+  }
+
+  void download() async {
+    var myvideourl = await flutterInsta.downloadReels(reelController.text);
+    final taskId = await FlutterDownloader.enqueue(
+      url: '$myvideourl',
+      savedDir: '/sdcard/Download',
+      showNotification: true,
+      // show download progress in status bar (for Android)
+      openFileFromNotification:
+          true, // click on notification to open downloaded file (for Android)
+    ).whenComplete(() {
+      setState(() {
+        downloading = false; // set to false to stop Progress indicator
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,14 +76,14 @@ class _ReelsDownloadState extends State<ReelsDownload> {
               child: new TextField(
                 decoration: new InputDecoration(
                     border: new OutlineInputBorder(
-                      borderSide: new BorderSide(color: Colors.white),
+                      borderSide: new BorderSide(color: Colors.transparent),
                       borderRadius: const BorderRadius.all(
                         const Radius.circular(50.0),
                       ),
                     ),
                     filled: true,
                     hintText: "HTTPS://WWW.INSTAGRAM.COM/...",
-                    fillColor: Colors.white70),
+                    fillColor: Colors.transparent),
               ),
             ),
             SizedBox(
@@ -52,12 +93,23 @@ class _ReelsDownloadState extends State<ReelsDownload> {
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(18.0),
                   side: BorderSide(color: Colors.white)),
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  downloading = true; //set to true to show Progress indicator
+                });
+                download();
+              },
               color: Colors.white,
               textColor: Color(0xffD0009E),
               child: Text("Download".toUpperCase(),
                   style: TextStyle(fontSize: 14)),
             ),
+            downloading
+                ? Center(
+                    child:
+                        CircularProgressIndicator(), //if downloading is true show Progress Indicator
+                  )
+                : Container()
           ],
         ),
       ),
